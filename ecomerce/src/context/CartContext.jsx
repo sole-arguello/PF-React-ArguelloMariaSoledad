1a7c85b4eec89bb335d92acbada056db210d48e1
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-04-28 20:00:01 -0300

    agrego funcionalidad a btn X, que sume el total y subtotal

--- ecomerce/src/context/CartContext.jsx
+++ ecomerce/src/context/CartContext.jsx
@@ -13,13 +13,30 @@ export const CartProvider = ({children}) => {
     console.log(cartList)
 
     //agregar producto
-    const addItem = (newProducts) => {
+    const addItem = (newProduct) => {
 
-        setCartList([...cartList, newProducts])
+        //Busca el índice del primer elemento en el array carList que tenga el mismo id que el nuevo elemento
+        //si el indice es -1 quiere decir que no esta y lo agrega y si no es -1 existe y se debe actualizar
+        const index = cartList.findIndex(prod => prod.id === newProduct.id)
+        console.log(index)
+        if(index === -1){
+            //Si !id al que esta se agrega el nuevo elemento al carrito
+            setCartList([...cartList, newProduct])
+        }else{
+            //Si el id es = se actualiza su cantidad en lugar de agregar un nuevo elemento
+            const actualizoCartList = [...cartList]
+            actualizoCartList[index].cantidad += newProduct.cantidad
+            setCartList(actualizoCartList)
+        }
+        
 
     }
 
     //remover item
+    const removeItem = (itemId) =>{
+        const item = cartList.filter( prod => prod.id !== itemId)
+        setCartList(item)
+    }
 
     //vaciar carrito
     const clearCart = () => {
@@ -32,7 +49,7 @@ export const CartProvider = ({children}) => {
             cartList,
             addItem,
             clearCart,
-
+            removeItem
         }}>
             { children }
         </CartContext.Provider>
@@ -40,10 +57,3 @@ export const CartProvider = ({children}) => {
 }
 
 
-        // const existsItem = cartList.some(prod => prod.id === itemId)
-        // console.log(existsItem)
-        // if(!existsItem){
-        // console.log(setCartList([...cartList, newProducts]))
-        // }else{
-        //     console.log("producto repetido")
-        // }
