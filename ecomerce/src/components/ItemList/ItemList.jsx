aed41beac77dcee4884059d2496000d33c26ec30
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-05 00:54:25 -0300

    corrijo funcion para envolver con memo

--- ecomerce/src/components/ItemList/ItemList.jsx
+++ ecomerce/src/components/ItemList/ItemList.jsx
@@ -2,18 +2,29 @@
 import { memo } from 'react'
 import Item from '../Item/Item'
 
-memo(
-  function ItemList({productos}) {
-    return (
-      <div className='row g-3 justify-content-center'>
-  
+const ItemList = memo ( ( { productos } )  => {
+  console.log('ItemList')
+  return (
+    <div className='row g-3 justify-content-center'>
+
         {productos.map( prod => <Item key={prod.id} {...prod}/>)}
-  
-      </div>
-    )
-  }
-
-) 
-
+    </div>
+  )
+}
+)
 export default ItemList
+// memo(
+//   function ItemList({productos}) {
+//     return (
+//       <div className='row g-3 justify-content-center'>
+  
+//         {productos.map( prod => <Item key={prod.id} {...prod}/>)}
+  
+//       </div>
+//     )
+//   }
+
+// ) 
+
+
 
