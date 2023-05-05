e96f4ca94b291680443426367ec0cbe57199f40d
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-05 19:58:42 -0300

    agrego logica al boton

--- ecomerce/src/components/CartWiget/CartWiget.jsx
+++ ecomerce/src/components/CartWiget/CartWiget.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../CartWiget/cartWiget.css'
+import { Link } from 'react-router-dom'
+import { CartContext } from '../../context/CartContext'
 //import '../CartWiget/cartWiget.scss'
 function CartWiget() {
+
+  const { totalEnCarrito } = useContext(CartContext)
   return (
-    <div className='cartWiget d-flex border border-dark p-1 rounded'>
-       <img className='mx-2 ' src="/public/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
-       <h5 className='mx-1'>0</h5>
+    <div className='cartWiget border border-dark p-1 rounded' 
+    style={{display: totalEnCarrito > 0 ? 'block' : 'none'}}>
+
+      <Link to='/cart' className='d-flex text-decoration-none text-dark'>
+        <img className='mx-2 ' src="/public/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
+        <span className=' fs-5 fw-semibold mx-1'>{totalEnCarrito}</span>
+      </Link>
+       
     </div>
   )
 }
