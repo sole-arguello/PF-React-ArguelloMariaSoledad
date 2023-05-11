23d22e576fa14d20fc30e78864e4609e1fafeb78
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 00:44:21 -0300

    colrrijo variables

--- ecomerce/src/components/ItemList/ItemList.jsx
+++ ecomerce/src/components/ItemList/ItemList.jsx
@@ -1,13 +1,12 @@
-
 import { memo } from 'react'
 import Item from '../Item/Item'
 
-const ItemList = memo ( function ( { productos } ) {
+const ItemList = memo ( function ( { product } ) {
   console.log('ItemList')
   return (
     <div className='row g-3 justify-content-center'>
 
-        {productos.map( prod => <Item key={prod.id} {...prod}/>)}
+        {product.map( prod => <Item key={prod.id} {...prod}/>)}
     </div>
   )
 }
