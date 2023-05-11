be9ed0269f5472b274f6490756d50c1db8850886
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-10 23:31:35 -0300

    aplico estilos

--- ecomerce/src/components/CheckoutForm/CheckoutForm.jsx
+++ ecomerce/src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,24 +1,23 @@
-import { Link } from "react-router-dom"
 
-
-function CheckoutForm({handleOnChange, generarOrden, dataForm}) {
+function CheckoutForm({handleForm, generateOrder, dataForm}) {
 
     
   return (
-    <div>
-      <form onSubmit={generarOrden} className=''>
-        {/* manejar el input desde un estado con value*/}
-        <input onChange={handleOnChange} type="text" name='name' value={dataForm.name} placeholder='Ingrese el Nombre'/>
+    <>
+      <form onSubmit={generateOrder} className=' d-flex flex-column gap-5'>
+       
+        <input onChange={handleForm} type="text" name='name' value={dataForm.name} placeholder='Ingrese el Nombre'/>
 
-        <input onChange={handleOnChange} type="text" name='phone' value={dataForm.phone} placeholder='Ingrese el Telefono'/>
+        <input onChange={handleForm} type="text" name='phone' value={dataForm.phone} placeholder='Ingrese el Telefono'/>
 
-        <input onChange={handleOnChange} type="text" name='email' value={dataForm.email} placeholder='Ingrese el email'/>
+        <input onChange={handleForm} type="text" name='email' value={dataForm.email} placeholder='Ingrese el email'/>
 
-        {/* <input type="text" /> validar el mail*/}
-        <button type="submit">Finalizar Compra</button>
+        <input onChange={handleForm} type="text" name='email' value={dataForm.email} placeholder='Confirme el email'/>
+
+        <button className="btn btn-outline-secondary w-50" type="submit">Finalizar Compra</button>
 
       </form>       
-    </div>
+    </>
   )
 }
 
