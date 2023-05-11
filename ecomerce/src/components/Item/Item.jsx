4a612703491b572fc1ce959d2460ea009f4123d4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 09:02:26 -0300

    corrijo variables y limpio codigo

--- ecomerce/src/components/Item/Item.jsx
+++ ecomerce/src/components/Item/Item.jsx
@@ -1,17 +1,18 @@
+import { title } from 'process'
 import { memo } from 'react'
 import { Button, Card, CardGroup } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 
-  const Item = memo ( function ( {id, titulo, img, precio, categoria} ) {
+  const Item = memo ( function ( {id, title, img, price, category} ) {
     return (
   
       <CardGroup className='col-lg-3 col-md-6 col-sm-10'>
           <Card className="card w-100 mh-100"  key={id}>
               <Card.Img variant="top" src={img} alt="imagen card" />
               <Card.Body >
-                <Card.Title className='fs-4'>{categoria} {titulo}</Card.Title>
-                <Card.Text>Precio: ${precio}</Card.Text>
+                <Card.Title className='fs-4'>{category} {title}</Card.Title>
+                <Card.Text>Precio: ${price}</Card.Text>
               </Card.Body>
               <Card.Footer className="card-footer">
                 <Link to={`/item/${id}`}>
