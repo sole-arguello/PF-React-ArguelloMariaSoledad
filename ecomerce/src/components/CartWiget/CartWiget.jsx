25ece25e7bd8587f0ded05bfc9554b5111844e67
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 09:04:11 -0300

    agrego nuevamente svg

--- ecomerce/src/components/CartWiget/CartWiget.jsx
+++ ecomerce/src/components/CartWiget/CartWiget.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
+import '../CartWiget/cartWiget.css'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
-
-import '../CartWiget/cartWiget.css'
-
+//import '../CartWiget/cartWiget.scss'
 function CartWiget() {
 
   const { totalInCart } = useContext(CartContext)
   return (
-    <div className='cartWiget border border-dark p-1 rounded'>
+    <div className='cartWiget border border-dark p-1 rounded' 
+    style={{display: totalInCart > 0 ? 'block' : 'none'}}>
 
       <Link to='/cart' className='d-flex text-decoration-none text-dark'>
         <img className='mx-2 ' src="/public/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
-        <span className=' fs-5 fw-semibold mx-1'>{totalInCart} style={{display: totalInCart > 0 ? 'block' : 'none'}}</span>
+        <span className=' fs-5 fw-semibold mx-1'>{totalInCart}</span>
       </Link>
        
     </div>
