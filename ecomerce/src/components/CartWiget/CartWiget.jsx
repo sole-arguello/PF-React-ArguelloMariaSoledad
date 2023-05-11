e898ab3f123dce26b741dc70eb09df97c1406374
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 10:28:58 -0300

    limpio y acomodo codigo

--- ecomerce/src/components/CartWiget/CartWiget.jsx
+++ ecomerce/src/components/CartWiget/CartWiget.jsx
@@ -2,17 +2,16 @@ import React, { useContext } from 'react'
 import '../CartWiget/cartWiget.css'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
-//import '../CartWiget/cartWiget.scss'
+
 function CartWiget() {
 
   const { totalInCart } = useContext(CartContext)
   return (
-    <div className='cartWiget border border-dark p-1 rounded' 
-    style={{display: totalInCart > 0 ? 'block' : 'none'}}>
+    <div className='cartWiget border border-dark p-1 rounded'>
 
       <Link to='/cart' className='d-flex text-decoration-none text-dark'>
         <img className='mx-2 ' src="/public/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
-        <span className=' fs-5 fw-semibold mx-1'>{totalInCart}</span>
+        <span className=' fs-5 fw-semibold mx-1' style={{display: totalInCart() > 0 ? 'block' : 'none'}}>{totalInCart()}</span>
       </Link>
        
     </div>
