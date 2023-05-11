6cd9675fe1f6d853240c99bdd0c9a6f211953c6a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 00:43:24 -0300

    limpio codigo y corrijo variables

--- ecomerce/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ ecomerce/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,29 +2,27 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Container, Spinner } from 'react-bootstrap'
 import ItemDetail from '../ItemDitail/ItemDetail'
-import { getProductoById } from '../../utils/mockFetch'
 import { doc, getDoc, getFirestore } from 'firebase/firestore'
 
 
 
 function ItemDetailContainer({greeting}) {
 
-    const [ producto, setProducto ] = useState(null)
+    const [ product, setProduct ] = useState(null)
     const [ isLoading, setIsLoading ] = useState(true)
 
     const { prodId } = useParams()
-    //console.log(prodId)
+    
 
     useEffect( () => {
       setTimeout( () => {
-        //guardo mi db
-        const dbFirestore = getFirestore()
-        //guardo 1 producto por su id
-        const queryDoc = doc(dbFirestore, 'productos', prodId)
         
-        //traigo un producto por su id
+        const dbFirestore = getFirestore()
+        
+        const queryDoc = doc(dbFirestore, 'product', prodId)
+        
         getDoc(queryDoc)
-          .then(resp => setProducto(({ id: resp.id, ...resp.data()})))
+          .then(resp => setProduct(({ id: resp.id, ...resp.data()})))
           .catch( (err) => console.log(err))
           .finally(() => setIsLoading(false))
       }, 1000)  
@@ -34,7 +32,7 @@ function ItemDetailContainer({greeting}) {
         <h1 className='py-5 text-center'>{ greeting }</h1>
         { isLoading 
         ? <div className='text-center'><Spinner animation="border" variant="warning" /></div> 
-        : <ItemDetail {...producto}/> }  
+        : <ItemDetail {...product}/> }  
     </Container>
   )
 }
