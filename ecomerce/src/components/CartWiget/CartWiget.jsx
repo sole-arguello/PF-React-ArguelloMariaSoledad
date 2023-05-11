4b3edb3a1e880d2fb925640b8120eada5be45987
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-11 01:04:25 -0300

    corrijo variables y limpio codigo

--- ecomerce/src/components/CartWiget/CartWiget.jsx
+++ ecomerce/src/components/CartWiget/CartWiget.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
-import '../CartWiget/cartWiget.css'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
-//import '../CartWiget/cartWiget.scss'
+
+import '../CartWiget/cartWiget.css'
+
 function CartWiget() {
 
-  const { totalEnCarrito } = useContext(CartContext)
+  const { totalInCart } = useContext(CartContext)
   return (
-    <div className='cartWiget border border-dark p-1 rounded' 
-    style={{display: totalEnCarrito > 0 ? 'block' : 'none'}}>
+    <div className='cartWiget border border-dark p-1 rounded'>
 
       <Link to='/cart' className='d-flex text-decoration-none text-dark'>
         <img className='mx-2 ' src="/public/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
-        <span className=' fs-5 fw-semibold mx-1'>{totalEnCarrito}</span>
+        <span className=' fs-5 fw-semibold mx-1'>{totalInCart} style={{display: totalInCart > 0 ? 'block' : 'none'}}</span>
       </Link>
        
     </div>
