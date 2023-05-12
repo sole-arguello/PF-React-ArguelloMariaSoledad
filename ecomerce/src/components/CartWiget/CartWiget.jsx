beaaee0fbf6ef4edc6cad7872938972f52bf6720
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-05-12 01:13:53 -0300

    ..

--- ecomerce/src/components/CartWiget/CartWiget.jsx
+++ ecomerce/src/components/CartWiget/CartWiget.jsx
@@ -10,7 +10,7 @@ function CartWiget() {
     <div className='cartWiget border border-dark p-1 rounded'>
 
       <Link to='/cart' className='d-flex text-decoration-none text-dark'>
-        <img className='mx-2 ' src="dist/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
+        <img className='mx-2 ' src="./dist/cart4.svg" alt="" style={{ width: '1.5rem'}}/>
         <span className=' fs-5 fw-semibold mx-1' style={{display: totalInCart() > 0 ? 'block' : 'none'}}>{totalInCart()}</span>
       </Link>
        
